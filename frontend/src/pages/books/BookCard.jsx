2d8ff5b7c0e2835b345ebb1051afe655ec5bded4
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-02-22 11:04:06 +0600

    d5

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -1,30 +1,45 @@
+import React from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import { getImgUrl } from "../../utils/getImgUrl";
-const BookCard = (book) => {
+
+import { Link } from "react-router-dom";
+
+const BookCard = ({ book }) => {
   return (
     <div className=" rounded-lg transition-shadow duration-300">
       <div className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4">
-        <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
-          <a href="/">
+        <div className="sm:h-72 sm:flex-shrink-0   rounded-md">
+          <Link to={`/books/${book._id}`}>
             <img
-              src={`${getImgUrl(book.coverImage)}`}
+              src={`${getImgUrl(book?.coverImage)}`}
               alt=""
               className="w-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
             />
-          </a>
+          </Link>
         </div>
 
         <div>
-          <a href="/">
+          <Link to={`/books/${book._id}`}>
             <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
-              {book.title}
+              {book?.title}
             </h3>
-          </a>
-          <p className="text-gray-600 mb-5">Book Description</p>
-          <p className="font-medium mb-5">
-            $80 <span className="line-through font-normal ml-2">$100</span>
+          </Link>
+          <p className="text-gray-600 mb-5">
+            {book.description.length > 80
+              ? `${book.description.slice(0, 80)}..`
+              : book.description}
           </p>
-          <button className="btn-primary px-6 space-x-1 flex items-center gap-1 ">
+          <p className="font-medium mb-5">
+            ${book?.newPrice}{" "}
+            <span className="line-through font-normal ml-2">
+              ${book?.oldPrice}
+            </span>
+          </p>
+          <button
+            onClick={() => handleAddToCart(book)}
+            className=" bg-amber-500  px-12 py-2 rounded-md text-base font-secondary font-bold hover:bg-secondary hover:text-white transition-all duration-200 cursor-pointer;
+ space-x-1 flex items-center gap-1 "
+          >
             <FiShoppingCart className="" />
             <span>Add to Cart</span>
           </button>
--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import BookCard from "../books/BookCard";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 const categories = [
   "Choose a genre",
   "Business",
@@ -47,9 +55,38 @@ const TopSellers = () => {
         </select>
       </div>
 
-      {filteredBooks.map((book, index) => (
-        <BookCard key={index} book={book} />
-      ))}
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {filteredBooks.length > 0 &&
+          filteredBooks.map((book, index) => (
+            <SwiperSlide key={index}>
+              <BookCard book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 };
